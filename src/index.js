6eb321f6091de9142d00fb9eb62d3cdf3c34e4d9
Jimmy <dabrowski.mac.1@gmail.com>
2022-04-15 16:27:35 +0200

    i added switch for changing units, search mostly works need to fix bug occuring when user rejects geolaction

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./style.css";
-import showData from "./show data";
+import showData from "./show-data";
 import showIcon from "./images";
 import classMenager from "./class-menager";
+import searchByCityName from "./search-city";
 
 async function getData() {
+  let units = "metric";
+  let counter = 0;
+  document.querySelector(".units").textContent = `UNITS: ${units}`;
+
   // IF USER REJECT GEOLOCATION SHOW DEFAULT:
   const getDefaultWeatherDataMetric = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b28f3971f9895a1eb3ed3d2c2b3fdd63&units=metric`,
@@ -13,13 +18,16 @@ async function getData() {
   });
   const defaultWeatherDataMetric = await getDefaultWeatherDataMetric.json();
 
-  // GET USER POSITION
+  // GET USER COORDINATES
   const position = await new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   }).catch((error) => {
     console.log(error);
-    showData(defaultWeatherDataMetric);
+    showData(defaultWeatherDataMetric, units);
+    showIcon(defaultWeatherDataMetric.weather[0].icon);
+    classMenager(defaultWeatherDataMetric);
   });
+  console.log(position);
   //FETCH WEATHER DATA WITH API
   const getWeatherDataMetric = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${await position.coords
@@ -45,12 +53,44 @@ async function getData() {
   let weatherData = weatherDataMatric;
 
   // CREATE DIVS AND POPULATRE THEM WITH DATA FROM API
-  showData(weatherData);
-
-  console.log(weatherData);
-
+  showData(weatherData, units);
   showIcon(weatherData.weather[0].icon);
   classMenager(weatherData);
+
+  //EVENT FOR SEARCH FORM
+  document.getElementById("search-btn").addEventListener("click", async () => {
+    weatherData = await searchByCityName(units);
+    await showData(weatherData, units);
+    await showIcon(weatherData.weather[0].icon);
+    classMenager(weatherData);
+    counter++;
+  });
+
+  document
+    .getElementById("switch-units")
+    .addEventListener("click", async () => {
+      console.log(weatherData);
+      if (units === "metric") {
+        if (counter === 0) {
+          weatherData = weatherDataImperial;
+        } else weatherData = await searchByCityName(units);
+        units = "imperial";
+
+        showData(weatherData, units);
+        document.querySelector(".units").textContent = `UNITS: ${units}`;
+        console.log(units);
+      } else {
+        console.log(weatherData);
+        if (counter === 0) {
+          weatherData = weatherDataMatric;
+        } else weatherData = await searchByCityName(units);
+        units = "metric";
+        showData(weatherData, units);
+        document.querySelector(".units").textContent = `UNITS: ${units}`;
+      }
+    });
+
+  console.log(weatherData);
 }
 
 getData();
---
+++ src/show-data.js
@@ -0,0 +1,73 @@
+import windDirectionFunction from "./wind-direction";
+const showData = (data, units) => {
+  if (units === "metric") {
+    document.querySelector(".units").textContent = `UNITS: ${units}`;
+    const dataList = document.querySelector(".data-list");
+    dataList.innerHTML = "";
+    const temperature = document.querySelector(".temperature");
+
+    const cityAndCoutry = document.createElement("li");
+    cityAndCoutry.textContent = `${data.name}, ${data.sys.country}`;
+
+    const wind = document.createElement("ul");
+    wind.classList.add("wind-list");
+    wind.textContent = `Wind:`;
+    const windSpeed = document.createElement("li");
+    const windDirection = document.createElement("li");
+    windSpeed.textContent = `Speed: ${data.wind.speed} m/s`;
+    windDirection.textContent = `Direction: ${windDirectionFunction(data)}`;
+    wind.append(windSpeed, windDirection);
+
+    const minMaxTemp = document.createElement("ul");
+    minMaxTemp.textContent = "Temperature:";
+    const minTemp = document.createElement("li");
+    minTemp.textContent = `Min: ${Math.round(data.main.temp_min * 10) / 10} °C`;
+    const maxTemp = document.createElement("li");
+    maxTemp.textContent = `Max: ${Math.round(data.main.temp_max * 10) / 10} °C`;
+    minMaxTemp.append(minTemp, maxTemp);
+
+    const humidity = document.createElement("li");
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+    const visibility = document.createElement("li");
+    visibility.textContent = `Pressure: ${data.main.pressure} hPa`;
+
+    dataList.append(cityAndCoutry, wind, minMaxTemp, humidity, visibility);
+
+    temperature.textContent = `${Math.round(data.main.temp * 10) / 10} °F`;
+  } else {
+    document.querySelector(".units").textContent = `UNITS: ${units}`;
+    const dataList = document.querySelector(".data-list");
+    dataList.innerHTML = "";
+    const temperature = document.querySelector(".temperature");
+
+    const cityAndCoutry = document.createElement("li");
+    cityAndCoutry.textContent = `${data.name}, ${data.sys.country}`;
+
+    const wind = document.createElement("ul");
+    wind.classList.add("wind-list");
+    wind.textContent = `Wind:`;
+    const windSpeed = document.createElement("li");
+    const windDirection = document.createElement("li");
+    windSpeed.textContent = `Speed: ${data.wind.speed} mi/h`;
+    windDirection.textContent = `Direction: ${windDirectionFunction(data)}`;
+    wind.append(windSpeed, windDirection);
+
+    const minMaxTemp = document.createElement("ul");
+    minMaxTemp.textContent = "Temperature:";
+    const minTemp = document.createElement("li");
+    minTemp.textContent = `Min: ${Math.round(data.main.temp_min * 10) / 10} °F`;
+    const maxTemp = document.createElement("li");
+    maxTemp.textContent = `Max: ${Math.round(data.main.temp_max * 10) / 10} °F`;
+    minMaxTemp.append(minTemp, maxTemp);
+
+    const humidity = document.createElement("li");
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+    const visibility = document.createElement("li");
+    visibility.textContent = `Pressure: ${data.main.pressure} hPa`;
+
+    dataList.append(cityAndCoutry, wind, minMaxTemp, humidity, visibility);
+
+    temperature.textContent = `${Math.round(data.main.temp * 10) / 10} °F`;
+  }
+};
+export default showData;
