3dd45be3b433294886366cea3f3e079418e4bbb9
Jimmy <dabrowski.mac.1@gmail.com>
2022-04-22 13:12:55 +0200

    live preview

--- src/class-menager.js
+++
@@ -1,9 +0,0 @@
-// FUNCTION WHICH ADD CLASS OF WETHER FROM API DATA TO ICON DIV
-const classMenager = (data) => {
-  const container = document.getElementById("image-temp-container");
-  container.className = "";
-  const className = `${data.weather[0].main}`;
-  className.toLowerCase();
-  container.classList.add(`${className.toLowerCase()}`);
-};
-export default classMenager;
